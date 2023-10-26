2bdd2a1f8fa0c63e83b92e4282acf2fbbbb94489
ValgerdurEyja <142887183+ValgerdurEyja@users.noreply.github.com>
2023-10-26 14:56:38 +0000

    final

--- musicalbum_2.py
+++ musicalbum_2.py
@@ -1,18 +1,13 @@
-#from typing import Any
-
-
 class MusicAlbum:
     def __init__(self, band = "unknown band", title = "unknown", year = "unknown year") -> None:
         self.band = band
         self.title = title
         self.year = year
     
-    def set_album(self, band = None, title = None, year = None):
-        if band != None:
-            self.band = band
-        if title != None:
-            self.title = title
-        if year != None:
-            self.year = year
+    def set_album(self, band = "unknown band", title = "unknown", year = "unknown year"):
+        self.band = band
+        self.title = title
+        self.year = year
+
     def __str__(self) -> str:
-        return f"{self.band}, released {self.title} in {self.year}."
+        return f"Album {self.title} by {self.band}, released in {self.year}."
